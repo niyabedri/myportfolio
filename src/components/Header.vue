ca6af43007f1edf6eeb13cee5e913c74cf33bf02
niya_bedri <niya.bedri44@gmail.com>
2022-08-19 23:16:12 +0300

    :star: adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app-bar
+    style="padding: 0px 90px;"
+    app
+    color="white"
+    light
+    elevation="6"
+    elevation-on-scroll
+  >
+    <v-toolbar-title>
+      My portfolio
+    </v-toolbar-title>
+    <v-spacer />
+    <v-list class="d-flex align-center">
+      <v-list-item v-for="(menu, index) in menus" :key="index" :to="menu.route">
+        <v-list-item-title>{{ menu.title }}</v-list-item-title>
+      </v-list-item>
+      <v-btn text>
+        <v-icon>
+          mdi-magnify
+        </v-icon>
+      </v-btn>
+      <v-btn outlined color="success">Get Started</v-btn>
+    </v-list>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    menus: [
+      { title: "Home", route: "home" },
+      { title: "About me", route: "about" },
+      { title: "Portfolio", route: "portfolio" },
+      { title: "Project", route: "project" },
+      { title: "Gallery", route: "gallery" },
+      { title: "Contact", route: "contact" },
+    ],
+    //
+  }),
+};
+</script>
+
+<style></style>
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="mt-0 pt-0">
+    <v-row>
+      <v-col lg="6">
+        <h2 class="intro-text">Welcome to my Portfolio</h2>
+        <p class="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <v-btn color="success" rounded>Explore about me</v-btn>
+      </v-col>
+      <v-col lg="6">
+        <img width="100%" height="100%" src="../assets/Me.jpg" alt="" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h2.intro-text {
+  font-size: 60px;
+  font-weight: bold;
+  font-family: cursive;
+  color: #444;
+}
+
+p.description {
+}
+</style>
