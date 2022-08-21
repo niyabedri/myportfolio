7aeeca05070d362489e899fb180e2c989bae1dc5
niya_bedri <niya.bedri44@gmail.com>
2022-08-21 23:00:38 +0300

    :zap: add about and footer component for the website

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col lg="6">
+        <img src="../assets/dev.jpeg" alt="" />
+      </v-col>
+      <v-col lg="6">
+        <v-subheader class="text-h4">
+          About
+        </v-subheader>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nos.
+        </p>
+        <v-list>
+          <v-list-item>
+            <v-icon class="mr-3" color="success">
+              mdi-check-all
+            </v-icon>
+            <v-list-item-title
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-icon class="mr-3" color="success">
+              mdi-check-all
+            </v-icon>
+            <v-list-item-title
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-icon class="mr-3" color="success">
+              mdi-check-all
+            </v-icon>
+            <v-list-item-title
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nos.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-footer padless>
+    <v-col lg="12">
+      <v-row justify="center">
+        <v-col lg="4" class="text-center">
+          <v-toolbar-title> Web Burden </v-toolbar-title>
+          <v-card-text>
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-4 black--text"
+              icon
+            >
+              <v-icon size="24px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+          <v-subheader>
+            sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+            lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+            iaculis dui sollicitudi
+          </v-subheader>
+          <v-form class="mt-4">
+            <v-text-field label="Email" outlined dense />
+            <v-btn rounded color="success">Subscribe</v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
+      <div class="mt-10">
+        <v-divider> </v-divider>
+        <v-subheader>
+          @ Copyright Niya Bedri. All Rights Reserved
+          <v-spacer />
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-subheader>
+      </div>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+    };
+  },
+};
+</script>
+
+<style></style>
