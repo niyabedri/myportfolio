2808d091ad4ce706d9a4e17e6a694f1c7848338d
niya_bedri <niya.bedri44@gmail.com>
2022-08-21 23:02:09 +0300

    :zap: call created component in app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <Header />
     <v-main>
       <HeroComponent />
+      <AboutMe />
+      <Portfolio />
+      <Project />
+      <Gallery />
+      <Contact />
+      <Footer />
     </v-main>
   </v-app>
 </template>
@@ -10,10 +16,25 @@
 <script>
 import Header from "./components/Header.vue";
 import HeroComponent from "./components/HeroComponent.vue";
+import AboutMe from "./components/AboutMe.vue";
+import Portfolio from "./components/Portfolio.vue";
+import Project from "./components/Project.vue";
+import Gallery from "./components/Gallery.vue";
+import Contact from "./components/ContactMe.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
 
-  components: { Header, HeroComponent },
+  components: {
+    Header,
+    HeroComponent,
+    AboutMe,
+    Portfolio,
+    Project,
+    Gallery,
+    Contact,
+    Footer,
+  },
 
   data: () => ({
     //
